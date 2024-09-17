358367ee25b524a4727b5a0937f5ebd22950d99d
jacobchlebowski <jachlebowski@wpi.edu>
2024-09-17 15:06:03 -0400

    update score test

--- HW1/syllablast/src/model.test.ts
+++ HW1/syllablast/src/model.test.ts
@@ -1,6 +1,9 @@
 import { expect, test } from 'vitest'
 import { Coordinate, Syllable, Puzzle, Model } from './model.ts'
 import { configuration1, configuration2, configuration3 } from './puzzle.ts'
+import Home from './app/page.tsx'
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
 test('Coordinate', () => {
   let c1 = new Coordinate(2, 3)
@@ -177,3 +180,53 @@ test('undoSwap', () => {
 //   expect(chooseConfiguration(m,buttonName)).toBe(undefined)
 // })
 
+// test('Access GUI', async() => {
+//   const { getByTestId } = render(Home());
+
+//   const swapbutton = getByTestId('swapbutton');
+//   const canvasElement = getByTestId('canvas');
+
+//   // Verify that the swap button is initially disabled
+//   expect(swapbutton.disabled).toBeTruthy();
+
+//   // Simulate a canvas click (the coordinates are based on your game's requirements)
+//   fireEvent.click(canvasElement, { screenX: 436, screenY: 573, clientX: 184, clientY: 440 });
+
+//   // You can then assert how the state should change (e.g., swap button becomes enabled after some interactions)
+//   expect(swapbutton.disabled).toBeFalsy();
+// });
+
+
+
+test('updateScore', () => {
+  let m = new Model(configuration1)
+  let numRows = 4;
+  let numColumns = 4;
+  let selected = [];
+  let previousMoves = [];
+
+  let s1 = new Syllable(new Coordinate(0,0), "ter"), s2 = new Syllable(new Coordinate(0,1), "ate"), s3 = new Syllable(new Coordinate(0,2), "ble"), s4 = new Syllable(new Coordinate(0,3), "der"), s5 = new Syllable(new Coordinate(1,0), "fil"), s6 = new Syllable(new Coordinate(1,1), "in"), s7 = new Syllable(new Coordinate(1,2), "im"), s8 = new Syllable(new Coordinate(1,3), "i"), s9 = new Syllable(new Coordinate(2,0), "i"), s10 = new Syllable(new Coordinate(2,1), "late"), s11 = new Syllable(new Coordinate(2,2), "mac"), s12 = new Syllable(new Coordinate(2,3), "un"), s13 = new Syllable(new Coordinate(3,0), "u"), s14 = new Syllable(new Coordinate(3,1), "vis"), s15 = new Syllable(new Coordinate(3,2), "af"), s16 = new Syllable(new Coordinate(3,3), "wa");
+  var allSyllables:Array<Syllable> = [];
+  allSyllables.push(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16)
+
+  var allParentWords:Array<Array<string>> = [];
+  allParentWords.push(["in","vis","i","ble"],["im","mac","u","late"],["af","fil","i","ate"],["un","der","wa","ter"])
+
+  //test puzzle
+  let pz = new Puzzle(numRows, numColumns, allSyllables, selected, previousMoves, allParentWords);
+  m.puzzle = pz
+  m.puzzle.syllables = allSyllables;
+  
+
+  //update score before swap
+  m.updateScore()
+  expect(m.scoreCounter).toStrictEqual(0)
+
+  // //commence a swap between syllables[0] and [15] for +1 in score
+  let syllable3 = pz.allSyllables[0];
+  let syllable4 = pz.allSyllables[15];
+  m.swap(syllable3,syllable4);
+  m.updateScore()
+  expect(m.scoreCounter).toStrictEqual(0)
+
+})
